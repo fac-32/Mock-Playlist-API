0689a991b4a31412ab6e8ae2d47a2966b5fce143
TB <thrishbg15@gmail.com>
2025-11-18 15:47:14 +0000

    fix: endpoints

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,9 @@ import router from "./index.js";
 const app = express();
 app.use(express.json());
 const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send('Welcome to the Playlist API');
+});
 app.use('/api', router);
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
