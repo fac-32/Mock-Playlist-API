108ff90f1da803234d760a4b7031417b1b91f559
Kai Cheung <kaicheunga@gmail.com>
2025-11-18 17:17:48 +0000

    Fix server test

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -4,9 +4,8 @@ import app from "../src/server.ts";
 
 describe("GET /", () => {
   it('returns welcome message', async () => {
-    const response = await request(app)
-    .post('/')
+    const response = await request(app).get('/')
 
-    expect(response.status).toBe(404)
+    expect(response.status).toBe(200)
   })
 });
