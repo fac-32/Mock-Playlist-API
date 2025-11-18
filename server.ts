d119f42f6b528a376c9745303ae3fdfb05689dc5
tmachado <rosadelonfamily@gmail.com>
2025-11-18 14:46:46 +0000

    deleting

--- server.ts
+++ server.ts
@@ -1,16 +1,3 @@
-// setup server 
-import express from "express";
-import { router } from "./index";
 
-const app = express();
-app.use(express.json());
-
-const PORT = 8000;
-
-app.use('/api', router);
-
-app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`)
-})
 
 
